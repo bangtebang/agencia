2f403d2fe472215f7f05d02eafbe5d35788146f6
bangtebang <e.diaz09@uformail.cl>
2023-05-22 03:04:09 -0400

    subo clases generadas con visual

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,2 +1,33 @@
 public class Cliente {
+	private String nombre;
+	private int edad;
+	private String run;
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getRun() {
+		return this.run;
+	}
+
+	public void setRun(String run) {
+		this.run = run;
+	}
+
+	public Cliente(String nombre, int edad, String run) {
+		throw new UnsupportedOperationException();
+	}
 }
